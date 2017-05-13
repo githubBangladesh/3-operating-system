ff6b3f975e5a9f72fb323609880d818bfa201694
Sakib Rahman <sakib.rahman.0000@gmail.com>
2017-05-13 17:59:10 +0600

    updating project contents : v1.1--13-05-2017

--- git-cmds-for-github.sh
+++ git-cmds-for-github.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "organizing project structure : v1--13-05-2017"
+git commit -m "updating project contents : v1.1--13-05-2017"
 git remote add origin https://github.com/githubBangladesh/operating-system
 git remote -v
 git push origin master
