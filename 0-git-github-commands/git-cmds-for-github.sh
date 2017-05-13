6f7d88a34323a7a682b6662a233b4d0f73ca9c7b
Sakib Rahman <sakib.rahman.0000@gmail.com>
2017-05-13 18:19:06 +0600

    Updating Project : v1.1--13-05-2017

--- 0-git-github-commands/git-cmds-for-github.sh
+++ 0-git-github-commands/git-cmds-for-github.sh
@@ -1,8 +1,8 @@
 git add .
-git commit -m "Creating Initial Project Structure : v1--13-05-2017"
-git remote add origin https://github.com/githubBangladesh/operating-system
+git commit -m "Updating Project : v1.1--13-05-2017"
+git remote add origin https://github.com/build-software-together/operating-system
 git remote -v
 git push origin master
-git push --all -f https://github.com/githubBangladesh/operating-system
+git push --all -f https://github.com/build-software-together/operating-system
 
 #git pull https://github.com/githubBangladesh/operating-system
